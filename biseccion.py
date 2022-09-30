f00c89a61cbc0fd4a70a71ddad1dfc8bde5761c9
cortegons <88986744+cortegons@users.noreply.github.com>
2022-09-30 17:12:15 -0500

    Create biseccion.py

---
+++ biseccion.py
@@ -0,0 +1,31 @@
+def f(fun, x):
+    y = eval(fun)
+    return y
+
+def biseccion():
+    fun = input("ingresa tu función")
+    a = float(input("ingresa el límite inferior del intervalo"))
+    b = float(input("ingresa el límite superior del intervalo"))
+    tol = float(input("ingrese la tolerancia deseada"))
+    iteraciones = int(input("ingrese el número máximo de iteraciones"))
+    
+    i = 1
+    fa = f(fun, a)
+    fb = f(fun, b)
+    
+    while i <= iteraciones:
+        p = a + (b-a)/2
+        fp = f(fun, p)
+        print(p, "intervalo: ", "[", a, ",", b, "]")
+        if fp == 0 or (b-a)/2 < tol:
+            print("La solución es ", p, " después de ", i, " iteraciones")
+        if fa*fp > 0:
+            a = p
+            fa = fp
+        else:
+            b = p
+            fb = fp
+    if i >= iteraciones:
+        print("El método no converge")
+
+biseccion()
